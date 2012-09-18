c8060e22b2d439756b476bead78cebf2ccebeea0
Josh Benham <joshbenham@gmail.com>
2012-09-19 08:30:45 +1000

    Updated the setup file and added a LICENSE line

--- setup.py
+++ setup.py
@@ -1,18 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+#  setup.py
+#  coderwall
+#
+#  Created by Josh Benham.
+
+"""
+The package configuration for coderwall.
+"""
+
 from setuptools import setup
 
-setup(name='coderwall',
-  version='0.1',
-  description='CoderWall command line interface to retrieve users statistics',
-  long_description=open('README.md').read(),
-  classifiers=[
-    "Programming Language :: Python",
-  ],
-  author="Josh Benham",
-  author_email='joshbenham@gmail.com',
-  url='https://github.com/joshbenham/coderwall',
-  keywords='web python coderwall statistics cli terminal',
-  packages=['coderwall',],
-  scripts=['coderwall_r'],
-  install_requires=open('requirements.txt').read(),
-)
+with open('README.md') as f:
+    readme = f.read()
 
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read()
+
+setup(
+    name='coderwall',
+    version='0.0.1',
+    description='CoderWall command line interface to retrieve users statistics',
+    long_description=readme,
+    keywords='web python coderwall statistics cli terminal',
+    author='Josh Benham',
+    author_email='joshbenham@gmail.com',
+    url='https://github.com/joshbenham/coderwall',
+    license=license,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    packages=['coderwall'],
+    scripts=['coderwall_r'],
+    install_requires=requirements,
+)
